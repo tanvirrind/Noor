98a37d13546a645e48ba1d4d600f333a0c24b7a2
tanvirrind <59108332+tanvirrind@users.noreply.github.com>
2026-04-17 23:32:10 +0500

    Add Tailwind CSS configuration file

---
+++ tailwind.config.ts
@@ -0,0 +1,18 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brandGreen: "#144327",
+        brandGold: "#C8A962",
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
